90c08c780b2d0bddae527c4f92b81e63ab035d9a
Pavel Tyavin <pavel@tyavin.name>
2014-03-18 01:45:24 +0400

    hello jenkins#2

--- vortaro/main/settings/prod_test.py
+++ vortaro/main/settings/prod_test.py
@@ -1,6 +1,15 @@
 from vortaro.main.settings.prod import *
 
 INSTALLED_APPS += ('django_jenkins',)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'handlers': {
+    },
+    'loggers': {
+    },
+}
 JENKINS_TASKS = (
     # 'django_jenkins.tasks.run_pylint',
     # 'django_jenkins.tasks.run_pep8',
