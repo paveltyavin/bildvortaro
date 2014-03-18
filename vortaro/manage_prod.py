83563588d86ce434fa1e5782916000f83bc2ea7c
Pavel Tyavin <pavel@tyavin.name>
2014-03-19 00:37:53 +0400

    hello jenkins#8

--- vortaro/manage_prod.py
+++ vortaro/manage_prod.py
@@ -1,10 +1,10 @@
-#!./../../env/bin/python
+#!./home/vinograd19/vortaro/env/bin/python
 
 import os
 import sys
 
 if __name__ == "__main__":
-    sys.path.insert(0, os.path.abspath(os.path.join(__file__, '..', '..')))
+    sys.path.insert(0, '/home/vinograd19/vortaro/src/')
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vortaro.main.settings.prod")
     from django.core.management import execute_from_command_line
 
