8a6542b3612b06b7bd2bace2f7193d057e619526
Pavel Tyavin <pavel@tyavin.name>
2014-03-20 00:41:24 +0400

    add less

--- vortaro/app/admin.py
+++ vortaro/app/admin.py
@@ -1,8 +1,19 @@
-from django.contrib import  admin
-from vortaro.app.models import Word
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.sites.models import Site
+from vortaro.app.models import Word, Category
 
 
 class WordAdmin(admin.ModelAdmin):
     pass
 
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Word, WordAdmin)
+admin.site.register(Category, CategoryAdmin)
+
+admin.site.unregister(Site)
+admin.site.unregister(Group)
--- vortaro/app/models.py
+++ vortaro/app/models.py
@@ -1,10 +1,31 @@
 # coding=utf-8
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.text import slugify
+
+from django.db.models.signals import post_delete
+from sorl.thumbnail import delete
 
 
 class User(AbstractUser):
     pass
+
+
+def get_image_wrap(model):
+    def f(instance, filename, **kwargs):
+        ar = filename.split('.')
+        if len(ar) > 1:
+            name, extension = ar[-2:]
+        else:
+            name = filename
+            extension = ''
+        return '{model}/{name}.{extension}'.format(
+            model=model,
+            name=slugify(name),
+            extension=extension.lower(),
+        )
+
+    return f
 
 
 class Word(models.Model):
@@ -13,3 +34,31 @@ class Word(models.Model):
         verbose_name_plural = u'Слова'
 
     name = models.CharField(max_length=128, verbose_name=u'Имя')
+    category = models.ForeignKey('Category', verbose_name=u'Категория', blank=True, null=True, default=None,)
+    image = models.ImageField(
+        verbose_name=u'Изображение',
+        upload_to=get_image_wrap('word'),
+        default='',
+    )
+
+    def __unicode__(self):
+        return self.name
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = u'Категория'
+        verbose_name_plural = u'Катеоргии'
+
+    name = models.CharField(max_length=128, verbose_name=u'Имя')
+    image = models.ImageField(verbose_name=u'Изображение', upload_to=get_image_wrap('category'))
+    def __unicode__(self):
+        return self.name
+
+
+def delete_image(instance, **kwargs):
+    if instance.image:
+        delete(instance.image)
+
+
+post_delete.connect(delete_image, sender=Word)
