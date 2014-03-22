f2a3e60c221f413c267e8b2100afbd13290775c5
Pavel Tyavin <pavel@tyavin.name>
2014-03-22 19:43:39 +0400

    user admin on

--- vortaro/app/admin.py
+++ vortaro/app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.sites.models import Site
-from vortaro.app.models import Word, Category
+from vortaro.app.models import Word, Category, User
 
 
 class WordAdmin(admin.ModelAdmin):
@@ -12,8 +12,13 @@ class CategoryAdmin(admin.ModelAdmin):
     pass
 
 
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Word, WordAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(User, UserAdmin)
 
 admin.site.unregister(Site)
 admin.site.unregister(Group)
