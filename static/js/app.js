40a78ca0932fde3cc2e90517dbfcc885b5078661
Pavel Tyavin <pavel@tyavin.name>
2014-03-23 12:57:13 +0400

    CGHJUS

--- static/js/app.js
+++ static/js/app.js
@@ -73,6 +73,26 @@ define([
     initUI: function () {
       var _this = this;
       this.ui.search.on('keyup', function (ev) {
+        $(this).val(function (i, v) {
+          v = v.replace(/c[xX]/, 'ĉ');
+          v = v.replace(/C[xX]/, 'Ĉ');
+
+          v = v.replace(/j[xX]/, 'Ĵ');
+          v = v.replace(/J[xX]/, 'Ĵ');
+
+          v = v.replace(/u[xX]/, 'ŭ');
+          v = v.replace(/U[xX]/, 'Ŭ');
+
+          v = v.replace(/g[xX]/, 'ĝ');
+          v = v.replace(/G[xX]/, 'Ĝ');
+
+          v = v.replace(/s[xX]/, 'ŝ');
+          v = v.replace(/S[xX]/, 'Ŝ');
+
+          v = v.replace(/h[xX]/, 'ĥ');
+          v = v.replace(/H[xX]/, 'Ĥ');
+          return v;
+        });
         var search = this.value;
         _this.filter.set('search', search);
         _this.filter.set('category', undefined);
