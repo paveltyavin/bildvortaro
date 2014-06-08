ce983a788fb59a5ff73c408d9ec6488e0cd4386b
Pavel Tyavin <pavel.tyavin@msk.action.ru>
2014-06-08 15:10:48 +0400

    #

--- static/Gruntfile.js
+++ static/Gruntfile.js
@@ -4,6 +4,17 @@ module.exports = function (grunt) {
   var staticRoot = grunt.option('staticRoot');
   var srcDir = grunt.option('srcDir');
 
+  var processhtmlFiles = {};
+  processhtmlFiles[srcDir + 'templates/base.html'] = srcDir + 'vortaro/app/templates/base.html';
+
+  var cssminFiles = {};
+
+  cssminFiles[staticRoot + 'css/style-' + revision + '.css'] = [
+    'css/from-less.css', 'bower_components/jquery-file-upload/css/jquery.fileupload.css',
+    'bower_components/select2-amd/select2', 'bower_components/select2-bootstrap-css/select2-bootstrap.css'
+  ];
+
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     requirejs: {
@@ -36,58 +47,59 @@ module.exports = function (grunt) {
     less: {
       production: {
         files: {
-          'css/from-less.css':'less/style.less'
+          'css/from-less.css': 'less/style.less'
         }
       }
     },
     cssmin: {
       minify: {
-        files: {
-          '<%= staticRoot %>css/style-<%= revision %>.css':[
-            'css/from-less.css',
-            'bower_components/jquery-file-upload/css/jquery.fileupload.css',
-            'bower_components/select2-amd/select2',
-            'bower_components/select2-bootstrap-css/select2-bootstrap.css'
-          ]
-        }
-      }
-    },
-    clean: {
-      options: {
-        force: true
-      },
-      js: [
-        staticRoot + "js/"
-      ],
-      css: [
-        staticRoot + "css/"
-      ],
-      templates :[
-        srcDir + 'templates/'
-      ]
-    },
-    processhtml: {
-      options: {
-        data: {
-          revision: revision
-        }
-      },
-      dist: {
-        files: {
-          '<%= srcDir %>templates/base.html':'<%= srcDir %>vortaro/app/templates/base.html'
-        }
+        files: cssminFiles
       }
     }
-  });
+  }, clean
+  :
+  {
+    options: {
+      force: true
+    }
+  ,
+    js: [
+      staticRoot + "js/"
+    ], css
+  :
+    [
+      staticRoot + "css/"
+    ], templates
+  :
+    [
+      srcDir + 'templates/'
+    ]
+  }
+  ,
+  processhtml: {
+    options: {
+      data: {
+        revision: revision
+      }
+    }
+  ,
+    dist: {
+      files: processhtmlFiles
+    }
+  }
+}
+)
+;
 
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-requirejs');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-processhtml');
-  grunt.registerTask('default', [
-    'clean', 'requirejs', 'processhtml', 'less', 'cssmin', 'copy'
-  ]);
+grunt.loadNpmTasks('grunt-contrib-copy');
+grunt.loadNpmTasks('grunt-contrib-requirejs');
+grunt.loadNpmTasks('grunt-contrib-less');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+grunt.loadNpmTasks('grunt-contrib-clean');
+grunt.loadNpmTasks('grunt-processhtml');
+grunt.registerTask('default', [
+  'clean', 'requirejs', 'processhtml', 'less', 'cssmin', 'copy'
+]);
 
-};
+}
+;
