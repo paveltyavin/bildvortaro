c5970815b4aca3dd1f5877afbbef1a09901e662a
Pavel Tyavin <pavel.tyavin@msk.action.ru>
2015-06-12 02:45:36 +0300

    #

--- conf/build-worked.sh
+++ conf/build-worked.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
-
+export PRODUCTION="1";
 source /home/vinograd19/vortaro/env/bin/activate;
 
 cd /home/vinograd19/vortaro/src;
 git reset --hard HEAD;
 git pull -q;
 
-pip install -r requirements/prod.txt;
+pip install -r requirements/base.txt;
 
-cd /home/vinograd19/vortaro/src/vortaro;
-./manage_prod.py migrate --noinput;
+cd /home/vinograd19/vortaro/src/;
+python manage.py migrate --noinput;
 
-cd /home/vinograd19/vortaro/src/static/;
-bower install;
+cd /home/vinograd19/vortaro/src/frontend;
+gulp;
 
-cd /home/vinograd19/vortaro/src/vortaro;
-./manage_prod.py collectstatic --noinput;
-
-cd /home/vinograd19/vortaro/src/static/;
-node ./node_modules/grunt-cli/bin/grunt --revision="v`git rev-parse --short HEAD`" --staticRoot="/static/vortaro/static/" --srcDir="/home/vinograd19/vortaro/src/";
+cd /home/vinograd19/vortaro/src/;
+python manage.py collectstatic --noinput;
 
 sudo supervisorctl restart vortaro;
 deactivate;
--- conf/start.sh
+++ conf/start.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 set -e
-DJANGODIR=/home/vinograd19/vortaro/src/vortaro
-DJANGO_SETTINGS_MODULE=vortaro.main.settings.prod
+DJANGO_SETTINGS_MODULE=main.settings.local
 
-LOGFILE=/home/vinograd19/vortaro/logs/gunicorn.log
-LOGDIR=$(dirname $LOGFILE)
-NUM_WORKERS=1
-# user/group to run as
-USER=vinograd19
-GROUP=vinograd19
-cd /home/vinograd19/vortaro/src/vortaro
 source /home/vinograd19/vortaro/env/bin/activate
-source /home/vinograd19/vortaro/conf/passwords.sh
 
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
-export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
-test -d $LOGDIR || mkdir -p $LOGDIR
-exec /home/vinograd19/vortaro/src/vortaro/manage_prod.py run_gunicorn \
-    --bind=localhost:8011 \
-    --workers=$NUM_WORKERS \
-    --pid=/home/vinograd19/vortaro/pids/gunicorn.pid \
-    --log-file=$LOGFILE \
-    --user=$USER \
-    --group=$GROUP \
-    --log-level=debug
+test -d /home/vinograd19/vortaro/pids/ || mkdir -p /home/vinograd19/vortaro/pids/
+test -d /home/vinograd19/vortaro/log/ || mkdir -p /home/vinograd19/vortaro/log/
+
+exec gunicorn \
+    --env DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE proj.wsgi \
+    --bind=localhost:8004 \
+    --workers=3 \
+    --pid=/home/vinograd19/vortaro/pids/gunicorn_prod.pid \
+    --log-file=/home/vinograd19/vortaro/log/gunicorn.log \
+    --timeout=600 \
+    --user=vinograd19 \
+    --group=vinograd19 \
+    --log-level=info
